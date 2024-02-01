8e730c5b0b081699aa5ba9b0567dd43b15cfd0cd
Alif <imalif.shaikh999@gmail.com>
2024-02-01 22:51:39 +0530

    list methods

---
+++ ListMethods/app.py
@@ -0,0 +1,12 @@
+numbers=[10,20,30,40,50]
+numbers.append(60)
+numbers.insert(0, -10)
+numbers.remove(60)
+print(numbers)
+numbers.clear()
+print(numbers)  #[]
+
+num=[10,20,30]
+print(20 in num) #to check if item exists in list or not
+#returns blue
+print(len(num))
