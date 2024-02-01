9aa1700c269e3343fbed7c98b1ccbab55560ee29
Alif <imalif.shaikh999@gmail.com>
2024-02-01 17:28:01 +0530

    strings in python

---
+++ Strings/app.py
@@ -0,0 +1,10 @@
+course="Python for software development"
+print(course.upper())  #YTHON FOR SOFTWARE DEVELOPMENT
+print(course.lower())
+print(course)  #Python for software development
+print(course.find("t"))  #returns index of 1st occurance
+print(course.find("for"))
+print(course.replace("for","4"))
+
+#in operator
+print("Python" in course)
