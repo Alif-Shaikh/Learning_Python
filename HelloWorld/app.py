fd722fe544a0e9d79f837ab4b8297c0b2a5d43f2
Alif <imalif.shaikh999@gmail.com>
2024-02-01 16:40:07 +0530

    User Inputs

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,1 +1,9 @@
 print("Hello World")
+#age =20
+
+age=30
+print(age)
+
+price=19.95
+first_name='ALIF'
+is_online=False
---
+++ UserInput/app.py
@@ -0,0 +1,14 @@
+name=input("what is your name?")
+print("Hello"+name)
+
+birth_year=input("Enter your birth year:")
+#input function always returns in string format
+#age=2020-birth_year  #error
+age=2024-int(birth_year)
+print(age)
+
+#functions to convert to tht specific datatype
+int()
+float()
+bool()
+str()
