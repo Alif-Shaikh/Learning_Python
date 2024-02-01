c7b59aa66a787f13164642c8aab7a11d5ffa3fcb
Alif <imalif.shaikh999@gmail.com>
2024-02-01 20:12:39 +0530

    if else statement

---
+++ ConditionalStatements/app.py
@@ -0,0 +1,10 @@
+temperature=3
+#indentation is treated as block in python
+if temperature<30 and temperature>20:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature<=20 and temperature>5:
+    print("Its nice day")
+elif temperature<5:
+    print("Its cold")
+print("Done")
