fa5dbe01b7824540d8796804a2ec363cd665261b
Alif <imalif.shaikh999@gmail.com>
2024-02-01 18:48:36 +0530

    operators and it's precedence

---
+++ OperatorPrecedence/app.py
@@ -0,0 +1,13 @@
+x=(10+2)*3
+print(x)
+
+#compaison operators-   <,<=,>,>=,!=,==
+y=3>2
+print(y)   #true
+
+#logical Operators
+price=25
+print(price>10 and price<30)   #true
+
+val=23
+print(not val==34)   #true
