797a22597b4ab9767ab18c5831ab2184616e2a9f
Alif <imalif.shaikh999@gmail.com>
2024-02-01 22:34:02 +0530

    list in python

---
+++ List/app.py
@@ -0,0 +1,8 @@
+names=["John","Bob","Mosh","Sam","Mary"]
+print(names)
+print(names[2])
+print(names[-1])   #Mary
+print(names[-2])   #Sam
+names[0]="jon"
+print(names)      #['jon', 'Bob', 'Mosh', 'Sam', 'Mary']
+print(names[0:3]) #['jon', 'Bob', 'Mosh'] returns new list
